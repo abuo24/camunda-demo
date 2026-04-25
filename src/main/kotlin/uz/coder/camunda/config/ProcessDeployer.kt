ad1903e1ef9058e5dd6c4799364d565139551b1c
abuo24 <mryediniofficial9924@gmail.com>
2026-04-25 18:04:46 +0500

    finish order-process flow

--- src/main/kotlin/uz/coder/camunda/config/ProcessDeployer.kt
+++ src/main/kotlin/uz/coder/camunda/config/ProcessDeployer.kt
@@ -1,13 +1,11 @@
 package uz.coder.camunda.config
 
 import io.camunda.zeebe.client.ZeebeClient
-import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
-@Slf4j
 @Component
 class ProcessDeployer(private val zeebeClient: ZeebeClient) {
 
